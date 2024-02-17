0e95eea08b398bb5dfe4841a2e59cf39b981fe86
luis.nunes <luisj2felipe09@gmail.com>
2024-02-16 23:30:06 -0300

    fix/useclient

--- frontend/src/components/Listofcontacts.tsx
+++ frontend/src/components/Listofcontacts.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import { useState, useEffect } from 'react'
 import axios from 'axios';
 
-export default async function Contacts() {
+export default function Contacts() {
     const [people, setPeople] = useState([]);
 
     useEffect(() => {
